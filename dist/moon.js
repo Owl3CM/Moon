6d46b190a3469aa6bcfa13bee8280ee8bb322973
Haider959 <saochiha@gmail.com>
2023-09-07 18:03:33 +0300

    v0.0.27

--- dist/moon.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { readFile } from "./owlFs.js";
-import { Controller } from "./controller.js";
-await Controller.init(JSON.parse(await readFile("./moonconfig.json")));
-await Controller.createStyles();
--- scripts/publish.js
+++ scripts/publish.js
@@ -5,11 +5,8 @@ async function run() {
   const versions = pak.version.split(".");
   const version = `${versions[0]}.${versions[1]}.${+versions[2] + 1}`;
   const removeDist = "rm -rf dist ";
-  const copyConfig = "cp ./moonconfig.json ./dist/moonconfig.json && cp ./moonconfig.schema.json ./dist/moonconfig.schema.json";
   const buildDist = "yarn build";
-  const buildJs = "yarn build-scriptsTs";
-  // insert this line to dist/moon.js #!/usr/bin/env node
-  // const addNodeEnvLine = `sed -i '1i #!/usr/bin/env node' dist/moon.js`;
+  const copyConfig = "cp ./moonconfig.json ./dist/moonconfig.json";
   const addComent = "git add .";
   const publish = `yarn publish --new-version ${version} --access public`;
   const gitStage = `git commit -m "v ${version}"`;
@@ -17,13 +14,11 @@ async function run() {
   try {
     await exec("clear");
     await exec(removeDist);
-    // await exec(copyConfig);
     await exec(buildDist);
-    await exec(buildJs);
-    // await exec(addNodeEnvLine);
+    await exec(copyConfig);
+    await exec(addComent);
     await exec(gitStage);
     await exec(publish);
-    await exec(addComent);
     await exec(gitPush);
     console.log("published!");
   } catch (error) {
