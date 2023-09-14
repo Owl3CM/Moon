94365e0b4d4128e83a76bb2b308355463071b55d
Haider959 <saochiha@gmail.com>
2023-09-14 20:20:46 +0300

    v0.0.64-beta.2

---
+++ dist/init.js
@@ -0,0 +1,13 @@
+import { fileExists, readFile } from "./owlFs.js";
+import { Controller, packagePath } from "./controller.js";
+import { copyFileSync } from "fs";
+let time = 0;
+while (!(await fileExists("node_modules/moon-style")) && time < 60) {
+    time++;
+    await new Promise((resolve) => setTimeout(resolve, 500));
+}
+if (!(await fileExists("./moon.config.json")))
+    await copyFileSync(`${packagePath}/moon.config.default.json`, "./moon.config.json");
+const config = await readFile("./moon.config.json");
+await Controller.init(JSON.parse(config));
+await Controller.createStyles();
